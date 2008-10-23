bf1d3ad06669a9d667e3f5089281f8ae0bf60c4e
Jo Hund <jhund@clearcove.ca>
2008-10-23 13:37:37 -0700

    Updated request-environment to newest standard

--- request-environment/app/controllers/application.rb
+++ request-environment/app/controllers/application.rb
@@ -1,21 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  around_filter :set_request_parameters
+  before_filter :set_request_environment
 
 private
 
   # stores parameters for current request
-  def set_request_parameters
-    Person.current = current_user # current_user is set by restful_authentication
-    Conference.current = Conference.new
+  def set_request_environment
+    User.current = current_user # current_user is set by restful_authentication
     # You would also set the time zone for Rails time zone support here:
     # Time.zone = Person.current.time_zone
-    
-    yield
-    
-    # TODO: do I need to reset current here? Is this unnecessary?
-    Person.current = nil
-    Conference.current = nil
   end
 
 end
